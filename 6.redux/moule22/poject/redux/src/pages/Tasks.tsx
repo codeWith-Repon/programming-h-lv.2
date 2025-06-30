28e12bb72c7af0d1479233d6b021c33db5710599
Md Repon <repon7253@gmail.com>
2025-06-30 20:47:21 +0600

    complite redux project

---
+++ 6.redux/moule22/poject/redux/src/pages/Tasks.tsx
@@ -0,0 +1,53 @@
+import { AddTaskModal } from '@/components/module/tasks/addTaskModal';
+import TaskCard from '@/components/module/tasks/TaskCard';
+
+import { Button } from '@/components/ui/button';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { filterOptons } from '@/constants/options';
+import { useGetTasksQuery } from '@/redux/api/baseApi';
+import type { ITask } from '@/types';
+
+import { useState } from 'react';
+
+const Tasks = () => {
+  const [open, setSetOpen] = useState(false);
+  const { data, isLoading, isError } = useGetTasksQuery(undefined, {
+    pollingInterval: 30000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+    refetchOnReconnect: true,
+  });
+
+  console.log({ data: data, isloading: isLoading, isError: isError });
+
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
+
+  return (
+    <div className='mx-auto max-w-7xl px-5 mt-20'>
+      <div className='flex justify-end gap-5 items-center'>
+        <h1 className='mr-auto'>Tasks</h1>
+        <Tabs defaultValue='all'>
+          <TabsList>
+            {filterOptons.map((option) => (
+              <TabsTrigger key={option} value={option.toLowerCase()}>
+                {option}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+        <Button onClick={() => setSetOpen(true)}>Add Task</Button>
+      </div>
+      <AddTaskModal open={open} onClose={() => setSetOpen(false)} />
+      <div className='space-y-5 mt-5'>
+        {!isLoading &&
+          data.tasks.map((task: ITask) => (
+            <TaskCard key={task._id} task={task} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
